b07c1def2dfcab3589d40d41da30c1220532ed82
sitmo <thijs@sitmo.com>
2022-04-25 01:44:36 +0200

    Bump version: 0.3.0 → 0.4.0

--- thermal/__init__.py
+++ thermal/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Thijs van den Berg"""
 __email__ = 'thijs@sitmo.com'
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 from thermal.resample import ResampleGmm, ResampleHist, ResampleKde
 from thermal.stats import anderson_test, ks_test
